729447460c95a7852eb6d153f8fa0a9c78e164e1
Joseguera <joseguera@uclaextension.edu>
2023-12-06 13:05:36 -0800

    added domain

--- src/index.tsx
+++ src/index.tsx
@@ -22,6 +22,10 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
+        path: "/redpoint",
+        element: <Home />,
+      },
+      {
         path: "/about",
         element: <About />,
       },
